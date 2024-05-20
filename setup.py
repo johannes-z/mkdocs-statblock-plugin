2b3cde4c99cee949714d60aee81dbc57d9a20e13
johannes-z <johannes.zwirchmayr@gmail.com>
2024-05-20 15:20:00 +0200

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     name="mkdocs_statblock_plugin",
-    version="0.1",
+    version="0.1.1",
     description="MkDocs plugin to format YAML within statblock superfences as HTML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/johannes-z/mkdocs-statblock-plugin.git",
     author="Johannes Zwirchmayr",
     author_email="johannes.zwirchmayr@gmail.com",
